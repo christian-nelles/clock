9b60909b8ad16f5c94d54f153e1067269f8bc44d
Keralte _ <christian.nelles@laplateforme.io>
2025-01-06 13:35:04 +0100

    ajout de la fonction afficher_heure

--- horloge/horloge.py
+++ horloge/horloge.py
@@ -7,17 +7,22 @@ def init():
 
 def boucle():
     global t
-    while True:
+    os.system('cls')
+    print(f"{t[0]:02}:{t[1]:02}:{t[2]:02}")
+    time.sleep(1)
+    t = (t[0], t[1], t[2] + 1)
+    if t[2] == 60:
+        t = (t[0], t[1] + 1, 0)
+    if t[1] == 60:
+        t = (t[0] + 1, 0, 0)
+    if t[0] == 24:
+        init()
+            
+def afficher_heure():
         os.system('cls')
         print(f"{t[0]:02}:{t[1]:02}:{t[2]:02}")
-        time.sleep(1)
-        t = (t[0], t[1], t[2] + 1)
-        if t[2] == 60:
-            t = (t[0], t[1] + 1, 0)
-        if t[1] == 60:
-            t = (t[0] + 1, 0, 0)
-        if t[0] == 24:
-            init()
-            
+
 init()
-boucle()
+while True:
+    boucle()
+    afficher_heure()
