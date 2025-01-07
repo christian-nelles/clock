cac991a5cd6ce4bffb847375c4ce74c90c16c6e4
Keralte _ <christian.nelles@laplateforme.io>
2025-01-07 11:34:01 +0100

    ajout de pause

--- horloge/horloge.py
+++ horloge/horloge.py
@@ -1,5 +1,6 @@
 import time
 import os
+import keyboard
 
 def init():
     global t
@@ -9,11 +10,10 @@ def boucle():
     global t
     os.system('cls')
     print(f"{t[0]:02}:{t[1]:02}:{t[2]:02}")
-    time.sleep(1)
     t = (t[0], t[1], t[2] + 1)
     if t[2] == 60:
         t = (t[0], t[1] + 1, 0)
-    if t[1] == 60:
+    if t[1] == 60:  
         t = (t[0] + 1, 0, 0)
     if t[0] == 24:
         init()
@@ -22,7 +22,16 @@ def afficher_heure():
         os.system('cls')
         print(f"{t[0]:02}:{t[1]:02}:{t[2]:02}")
 
+def pause():
+        input()
+        time.sleep(0.1)
+
 init()
 while True:
+    afficher_heure
+    start_time = time.time()
+    while time.time() - start_time < 1.5:
+        if keyboard.is_pressed('space'):
+            pause()
+    
     boucle()
-    afficher_heure()
