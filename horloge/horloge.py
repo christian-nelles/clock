16b40131dc65286466995e6b98527997cb3bd883
Keralte _ <christian.nelles@laplateforme.io>
2025-01-07 16:44:14 +0100

    correction de l'affichage dans la boucle

--- horloge/horloge.py
+++ horloge/horloge.py
@@ -8,8 +8,6 @@ def init():
 
 def boucle():
     global t
-    os.system('cls')
-    print(f"{t[0]:02}:{t[1]:02}:{t[2]:02}")
     t = (t[0], t[1], t[2] + 1)
     if t[2] == 60:
         t = (t[0], t[1] + 1, 0)
@@ -28,7 +26,7 @@ def pause():
 
 init()
 while True:
-    afficher_heure
+    afficher_heure()
     start_time = time.time()
     while time.time() - start_time < 1.5:
         if keyboard.is_pressed('space'):
