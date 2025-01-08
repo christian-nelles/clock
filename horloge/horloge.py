1622788600421700b8298f56264c31e796b80396
Keralte _ <christian.nelles@laplateforme.io>
2025-01-08 10:48:32 +0100

    refonte globale du code

--- horloge/horloge.py
+++ horloge/horloge.py
@@ -1,10 +1,6 @@
-import time
+from datetime import datetime
 import os
 import keyboard
-
-def init():
-    global t
-    t = (0, 0, 0) 
 
 def boucle():
     global t
@@ -14,22 +10,22 @@ def boucle():
     if t[1] == 60:  
         t = (t[0] + 1, 0, 0)
     if t[0] == 24:
-        init()
+        t = (0, 0, 0)
             
 def afficher_heure():
-        os.system('cls')
-        print(f"{t[0]:02}:{t[1]:02}:{t[2]:02}")
+    os.system('cls')
+    print(f"{t[0]:02}:{t[1]:02}:{t[2]:02}")
 
 def pause():
-        input()
-        time.sleep(0.1)
+    input()
 
-init()
+t = (0, 0, 0) 
 while True:
     afficher_heure()
-    start_time = time.time()
-    while time.time() - start_time < 1.5:
-        if keyboard.is_pressed('space'):
+    start_time = datetime.today().timestamp()
+    while datetime.today().timestamp() - start_time < 1.5:
+        if keyboard.is_pressed('backspace'):
             pause()
     
     boucle()
+    
